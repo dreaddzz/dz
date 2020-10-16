3dc89704f283627da2dc8755f5af1f3055aa51ab
ucishka <reic5yuldqba@mail.ru>
2020-10-17 00:21:00 +0300

    Added new method in MainFloat

--- myfloat/src/MainFloat.java
+++ myfloat/src/MainFloat.java
@@ -1,3 +1,11 @@
 public class MainFloat
 {
+    public static void main (String[] args)
+    {
+        MyFloat a = new MyFloat(421421, 3);
+        MyFloat b = new MyFloat(73467634, 2);
+
+        a.minUS(b);
+        a.sum(b);
+    }
 }
