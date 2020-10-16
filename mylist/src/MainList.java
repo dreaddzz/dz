d15918b53b98e38aa48bf45f43e63cfa083a2bad
ucishka <reic5yuldqba@mail.ru>
2020-10-17 00:14:36 +0300

    Add new method in MainList

--- mylist/src/MainList.java
+++ mylist/src/MainList.java
@@ -1,3 +1,19 @@
 public class MainList
 {
+    public static void main(String[] args)
+    {
+        MyList myList = new MyList();
+        myList.add(12);
+        myList.add(15);
+        myList.add(2);
+        myList.add(9);
+        myList.add(1);
+        myList.add(19);
+        System.out.println(myList);
+        System.out.println(myList.indexAt(2).toString());
+        myList.swap(2,4);
+        System.out.println(myList);
+        myList.sort();
+        System.out.println(myList);
+    }
 }
